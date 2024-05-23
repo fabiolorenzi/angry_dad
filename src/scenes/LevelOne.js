e0dd0bbf59a2b9ce969c16fabd50c24c534ed1a3
fabiolorenzi <fabiolorenzi13975@gmail.com>
2024-05-23 13:26:54 +0100

    developed sprite for player

--- src/scenes/LevelOne.js
+++ src/scenes/LevelOne.js
@@ -12,15 +12,20 @@ export class LevelOne extends Scene {
         this.load.addFile(new WebFontFile(this.load, "Permanent Marker"));
         this.load.image("background_levelOne", "background_at/Postapocalypce1/Bright/postapocalypse1.png");
         this.load.image("floor_levelOne", "background_at/Postapocalypce1/Bright/road.png");
+        this.load.spritesheet("player_idle", "simple_platformer_kit/1 Main Characters/1/Idle.png", {
+            frameWidth: 32,
+            frameHeight: 32
+        });
 
         this.initialTime = 90;
+        this.floors = this.physics.add.staticGroup();
+        this.player;
     }
 
     create() {
         this.add.image(512, 384, "background_levelOne");
 
-        let floors = this.physics.add.staticGroup();
-        floors.create(900, 300, "floor_levelOne");
+        this.floors.create(900, 300, "floor_levelOne");
 
         this.add.text(55, 40, "Time:", {
             fontFamily: "Permanent Marker", fontSize: 32, color: "#ffffff",
@@ -39,6 +44,20 @@ export class LevelOne extends Scene {
             stroke: "#000000", strokeThickness: 8,
             align: "center"
         }).setOrigin(0.5).setDepth(100);
+
+        this.anims.create({
+            key: "player_idle_anim",
+            frames: this.anims.generateFrameNumbers("player_idle"),
+            frameRate: 30,
+            repeat: -1
+        });
+
+        this.player = this.physics.add.sprite(280, 500, "player_idle");
+        this.player.play("player_idle_anim");
+        this.player.setScale(2.5);
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+        this.physics.add.collider(this.player, this.floors);
 
         setInterval(() => {
             this.initialTime -= 1;
