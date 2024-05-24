3fcfdb0dfc10f8034aa385923896b05b5957fb0a
fabiolorenzi <fabiolorenzi13975@gmail.com>
2024-05-24 09:56:50 +0100

    made the player sprite turning and developed animations for running and jumping

--- src/scenes/LevelOne.js
+++ src/scenes/LevelOne.js
@@ -12,7 +12,12 @@ export class LevelOne extends Scene {
         this.load.addFile(new WebFontFile(this.load, "Permanent Marker"));
         this.load.image("background_levelOne", "background_at/Postapocalypce1/Bright/postapocalypse1.png");
         this.load.image("floor_levelOne", "background_at/Postapocalypce1/Bright/road_cropped.png");
+
         this.load.spritesheet("player_idle", "simple_platformer_kit/1 Main Characters/1/Idle.png", {
+            frameWidth: 32,
+            frameHeight: 32
+        });
+        this.load.spritesheet("player_run", "simple_platformer_kit/1 Main Characters/1/Run.png", {
             frameWidth: 32,
             frameHeight: 32
         });
@@ -53,8 +58,15 @@ export class LevelOne extends Scene {
             repeat: -1
         });
 
+        this.anims.create({
+            key: "player_run_anim",
+            frames: this.anims.generateFrameNumbers("player_run"),
+            frameRate: 30,
+            repeat: -1
+        });
+
+
         this.player = this.physics.add.sprite(280, 650, "player_idle");
-        this.player.play("player_idle_anim");
         this.player.setScale(2.5);
         this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
@@ -72,10 +84,15 @@ export class LevelOne extends Scene {
     update() {
         if (this.cursors.left.isDown) {
             this.player.setVelocityX(-160);
+            this.player.setFlipX(true);
+            this.player.anims.play("player_run_anim", true);
         } else if (this.cursors.right.isDown) {
             this.player.setVelocityX(160);
+            this.player.setFlipX(false);
+            this.player.anims.play("player_run_anim", true);
         } else {
             this.player.setVelocityX(0);
+            this.player.play("player_idle_anim", true);
         }
 
         if (this.cursors.space.isDown && this.player.body.touching.down) {
