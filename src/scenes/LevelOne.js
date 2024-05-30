510215080083fc4aa36701f42e99912acef9af21
fabiolorenzi <fabiolorenzi13975@gmail.com>
2024-05-30 13:48:08 +0100

    developed logic for end game and cleaned code

--- src/scenes/LevelOne.js
+++ src/scenes/LevelOne.js
@@ -2,7 +2,8 @@ import { Scene } from "phaser";
 import GameUI from "../components/GameUI";
 import LifeUI from "../components/LifeUI";
 import Player from "../components/Player";
-import { createWall, removeBlocks } from "../components/Builder";
+import { removeBlocks } from "../components/Builder";
+import { createPlatformsLevelOne } from "../components/LevelsBuildings";
 import WebFontFile from "../components/WebFontFile";
 import { formatTime } from "../components/Timer";
 
@@ -37,7 +38,16 @@ export class LevelOne extends Scene {
 
         this.floors.create(950, 830, "floor_levelOne");
         this.pubs.create(12300, 630, "pub");
-        this.createPlatforms();
+        createPlatformsLevelOne(this.platforms);
+
+        //-------------------------------
+        // This code below is just for dev
+        const tar = 11200;
+        this.background.tilePositionX += tar;
+        this.platforms.children.entries.forEach(plat => plat.x -= tar);
+        this.pubs.children.entries.forEach(b => b.x -= tar);
+        //-------------------------------
+        //-------------------------------
 
         this.player = this.add.existing(new Player(this, 512, 650));
         this.camera = this.cameras.main;
@@ -81,97 +91,6 @@ export class LevelOne extends Scene {
         this.load.spritesheet("player_idle", "simple_platformer_kit/1 Main Characters/1/Idle.png", {frameWidth: 32, frameHeight: 32});
         this.load.spritesheet("player_run", "simple_platformer_kit/1 Main Characters/1/Run.png", {frameWidth: 32, frameHeight: 32});
         this.load.spritesheet("player_jump", "simple_platformer_kit/1 Main Characters/1/Jump.png", {frameWidth: 32, frameHeight: 32});
-    };
-
-    createPlatforms() {
-        createWall(this.platforms, 1200, 760, 6, true);
-        createWall(this.platforms, 1220, 660, 4, false);
-        createWall(this.platforms, 1500, 760, 9, true);
-        createWall(this.platforms, 1520, 640, 4, false);
-        createWall(this.platforms, 1700, 560, 8, false);
-        createWall(this.platforms, 1800, 760, 14, true);
-        createWall(this.platforms, 1960, 600, 2, false);
-        createWall(this.platforms, 2000, 760, 16, true);
-        createWall(this.platforms, 1660, 300, 14, false);
-        createWall(this.platforms, 2020, 460, 3, false);
-        createWall(this.platforms, 2080, 460, 15, true);
-        createWall(this.platforms, 2080, 160, 20, false);
-        createWall(this.platforms, 2560, 640, 37, true);
-        createWall(this.platforms, 2200, 300, 18, false);
-        createWall(this.platforms, 2100, 460, 18, false);
-        createWall(this.platforms, 2160, 640, 20, false);
-        createWall(this.platforms, 2340, 620, 3, true);
-        createWall(this.platforms, 2800, 760, 30, true);
-        createWall(this.platforms, 2580, 640, 3, false);
-        createWall(this.platforms, 2740, 500, 3, false);
-        createWall(this.platforms, 2580, 400, 3, false);
-        createWall(this.platforms, 2740, 250, 3, false);
-        createWall(this.platforms, 2900, 620, 35, true);
-        createWall(this.platforms, 2900, 640, 20, false);
-        createWall(this.platforms, 3460, 760, 20, true);
-        createWall(this.platforms, 3460, 260, 15, true);
-        createWall(this.platforms, 2920, 480, 2, false);
-        createWall(this.platforms, 3060, 380, 20, false);
-        createWall(this.platforms, 3140, 260, 16, false);
-        createWall(this.platforms, 2920, 260, 6, false);
-        createWall(this.platforms, 2920, 160, 16, false);
-        createWall(this.platforms, 3000, 140, 2, true);
-        createWall(this.platforms, 3460, 600, 50, false);
-        createWall(this.platforms, 3900, 580, 5, true);
-        createWall(this.platforms, 4600, 600, 40, true);
-        createWall(this.platforms, 4760, 600, 48, false);
-        createWall(this.platforms, 4620, 500, 50, false);
-        createWall(this.platforms, 5720, 760, 20, true);
-        createWall(this.platforms, 4760, 380, 48, false);
-        createWall(this.platforms, 4760, 280, 56, false);
-        createWall(this.platforms, 5720, 260, 20, true);
-        createWall(this.platforms, 5880, 660, 20, true);
-        createWall(this.platforms, 6200, 760, 32, true);
-        createWall(this.platforms, 5900, 660, 3, false);
-        createWall(this.platforms, 6140, 500, 3, false);
-        createWall(this.platforms, 5900, 360, 3, false);
-        createWall(this.platforms, 5900, 140, 15, false);
-        createWall(this.platforms, 6220, 140, 40, false);
-        createWall(this.platforms, 6360, 280, 40, false);
-        createWall(this.platforms, 6220, 420, 40, false);
-        createWall(this.platforms, 6360, 540, 40, false);
-        createWall(this.platforms, 6220, 660, 40, false);
-        createWall(this.platforms, 7140, 660, 40, true);
-        createWall(this.platforms, 7500, 760, 33, true);
-        createWall(this.platforms, 7160, 660, 3, false);
-        createWall(this.platforms, 7440, 600, 3, false);
-        createWall(this.platforms, 7160, 460, 3, false);
-        createWall(this.platforms, 7440, 400, 3, false);
-        createWall(this.platforms, 7160, 260, 3, false);
-        createWall(this.platforms, 8000, 100, 45, false);
-        createWall(this.platforms, 7980, 540, 23, true);
-        createWall(this.platforms, 7600, 660, 70, false);
-        createWall(this.platforms, 8000, 540, 45, false);
-        createWall(this.platforms, 8100, 420, 45, false);
-        createWall(this.platforms, 8000, 320, 45, false);
-        createWall(this.platforms, 8100, 220, 45, false);
-        createWall(this.platforms, 9000, 660, 33, true);
-        createWall(this.platforms, 9400, 760, 4, true);
-        createWall(this.platforms, 9550, 760, 8, true);
-        createWall(this.platforms, 9700, 580, 4, true);
-        createWall(this.platforms, 9850, 500, 4, true);
-        createWall(this.platforms, 9550, 300, 4, false);
-        createWall(this.platforms, 9800, 200, 4, false);
-        createWall(this.platforms, 10000, 200, 4, false);
-        createWall(this.platforms, 10200, 200, 4, false);
-        createWall(this.platforms, 10360, 240, 1, false);
-        createWall(this.platforms, 10560, 240, 1, false);
-        createWall(this.platforms, 10800, 340, 2, false);
-        createWall(this.platforms, 11000, 280, 2, false);
-        createWall(this.platforms, 11300, 180, 1, false);
-        createWall(this.platforms, 11400, 140, 1, false);
-        createWall(this.platforms, 11500, 760, 32, true);
-
-        // This code below is just for dev
-        const tar = 11200;
-        this.background.tilePositionX += tar;
-        this.platforms.children.entries.forEach(plat => plat.x -= tar);
-        this.pubs.children.entries.forEach(b => b.x -= tar);
     };
 
     moveGroups(isPlus) {
